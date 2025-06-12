16849ecd2a158a28d7e92436ad55e9f076013d9b
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-06-12 12:46:40 +0530

    fixed bug and basic app is running

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,4 @@
 import express from "express";
-import dotenv from "dotenv";
-import path from "path";
 import productRoutes from "./src/product/routes/product.routes.js";
 import {
   errorHandlerMiddleware,
@@ -9,9 +7,6 @@ import {
 import userRoutes from "./src/user/routes/user.routes.js";
 import cookieParser from "cookie-parser";
 import orderRoutes from "./src/order/routes/order.routes.js";
-
-const configPath = path.resolve("backend", "config", "uat.env");
-dotenv.config({ path: configPath });
 
 const app = express();
 app.use(express.json());
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import server from "./app.js";
+import dotenv from "dotenv";
+import path from "path";
+const configPath = path.resolve("backend", "config", "uat.env");
+dotenv.config({ path: configPath });
 import { connectDB } from "./config/db.js";
 
-const serverStar = server.listen(process.env.PORT, async (err) => {
+server.listen(process.env.PORT, async (err) => {
   if (err) {
     console.log(`server failed with error ${err}`);
   } else {
