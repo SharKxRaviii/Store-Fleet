0810c891bf73ac2b41d3c66894951b2b544f9449
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-06-18 23:22:38 +0530

    createNewOrder added

--- backend/src/order/controllers/order.controller.js
+++ backend/src/order/controllers/order.controller.js
@@ -6,4 +6,46 @@ import { ErrorHandler } from "../../../utils/errorHandler.js";
 
 export const createNewOrder = async (req, res, next) => {
   // Write your code here for placing a new order
+  const {
+    shippingInfo,
+    orderedItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  try {
+    if (
+      !shippingInfo ||
+      !orderedItems ||
+      !paymentInfo ||
+      !itemsPrice ||
+      !taxPrice ||
+      !shippingPrice ||
+      !totalPrice
+    ) {
+      return next(new ErrorHandler("Please provide all required fields", 400));
+    }
+
+    const orderData = {
+      shippingInfo,
+      orderedItems,
+      user: req.user._id,
+      paymentInfo,
+      paidAt: Date.now(),
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    };
+
+    const newOrder = await createNewOrderRepo(orderData);
+    res
+      .status(201)
+      .json({ success: true, message: "Order placed successfully", newOrder });
+  } catch (error) {
+    return next(new ErrorHandler(500, error));
+  }
 };
--- backend/src/order/model/order.repository.js
+++ backend/src/order/model/order.repository.js
@@ -2,4 +2,8 @@ import OrderModel from "./order.schema.js";
 
 export const createNewOrderRepo = async (data) => {
   // Write your code here for placing a new order
+  const newOrder = new OrderModel(data);
+
+  return await newOrder.save();
+
 };
